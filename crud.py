d3c5db575a69cf117b52438519f5da747c6b03fe
shprd <vladimir362616@gmail.com>
2025-09-19 20:20:15 +0300

    Going through ch3.1

--- crud.py
+++ crud.py
@@ -6,8 +6,17 @@ app = FastAPI()
 messages_db = {"0":"first post in FastAPI"}
 
 @app.get("/")
+async def get_all_messages() -> dict:
+    pass
+
+@app.get("/message/{message_id}")
 async def get_message(message_id: str) -> str:
     pass
 
-@app.post("/message")
-async def
+@app.post("/message/")
+async def create_message(message_id: str) -> str:
+    pass
+
+@app.put("/message/{message_id}")
+async def update_message(message_id: str, message: str) -> str:
+    pass
